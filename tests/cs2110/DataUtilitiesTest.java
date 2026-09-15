690001eeb17cee6092bc8cf4c670ca349598d0b5
Branny626 <0114019@student.isf.edu.hk>
2026-09-15 16:54:48 -0400

    Revert "yo"
    This reverts commit 423e7e8e270e683ebbccdbb53475a0a30e680a7f.
    
    helpppp
    :wq
    :q

--- tests/cs2110/DataUtilitiesTest.java
+++ tests/cs2110/DataUtilitiesTest.java
@@ -55,23 +55,6 @@ public class DataUtilitiesTest {
         assertEquals(6, binarySearch(views, key, BY_USER_ID, RIGHT));
     }
 
-    @DisplayName("WHEN the userID of the `key` is alphabetically after the userIDs of all of the "
-            + "`view`s, THEN `binarySearch()` with the BY_USER_ID Comparator returns 0")
-    @Test
-    public void testBinarySearchKeyLessThanViews() {
-        View[] views = new View[]{
-                new View("B", "V", LocalDateTime.now()),
-                new View("C", "V", LocalDateTime.now()),
-                new View("D", "V", LocalDateTime.now()),
-                new View("E", "V", LocalDateTime.now()),
-                new View("F", "V", LocalDateTime.now()),
-                new View("G", "V", LocalDateTime.now())
-        };
-        View key = new View("A", "V", LocalDateTime.now());
-        assertEquals(0, binarySearch(views, key, BY_USER_ID, LEFT));
-        assertEquals(0, binarySearch(views, key, BY_USER_ID, RIGHT));
-    }
-
     @DisplayName("WHEN multiple of the `views` records has the target videoID, THEN `binarySearch()` "
             + "with the BY_VIDEO_ID Comparator and LEFT search policy returns the index of the left most "
             + "view.")
